ac195b73dc46fe59f8d81d26e9d6afc2f6fc81d3
Austin-Draper <austin.draper@aol.com>
2020-10-20 17:30:49 -0500

    Add project files.

---
+++ RegisterWebAPI/Models/AuthenticationContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegisterWebAPI.Models
+{
+    //usually a model will inherit from DbContext class... but in this case we inherit from IdentityDbContext
+    public class AuthenticationContext : IdentityDbContext
+    {
+        public AuthenticationContext(DbContextOptions options):base(options)
+        {
+
+        }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    }
+}
